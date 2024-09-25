9d8e2ff764d05a86017746ce6dd12c9ec89113f4
Orual <orual.nbc@proton.me>
2024-09-24 20:22:30 -0400

    Made sure the soft keyboard will not show when the text field is focused and auto-focused the text field.

--- composeApp/src/commonMain/kotlin/com/xhbtech/speechapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/xhbtech/speechapp/App.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -13,6 +12,8 @@ import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSiz
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -20,17 +21,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.input.key.type
+import androidx.compose.ui.platform.LocalTextInputService
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-@OptIn(ExperimentalMaterial3WindowSizeClassApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 @Preview
 fun App() {
@@ -59,53 +63,62 @@ fun App() {
             verticalArrangement = Arrangement.Center
         ) {
             Spacer(Modifier.weight(1f))
-            OutlinedTextField(
-                value = textValue,
-                onValueChange = { newText: String ->
-                    textValue = newText
-                },
-                textStyle = textStyle,
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = Color.Transparent,
-                    disabledBorderColor = Color.Transparent,
-                    errorBorderColor = Color.Transparent,
-                    unfocusedBorderColor = Color.Transparent,
-                    cursorColor = Color.Transparent,
-                ),
-                placeholder = {
-                    Text(
-                        text = prevTextValue,
-                        style = textStyle,
-
-                        color = MaterialTheme.colorScheme.onSurface,
-                        modifier = Modifier.fillMaxWidth(),
-                    )
-                },
-
-                modifier = Modifier.onPreviewKeyEvent {
-                    when {
-                        (it.key == Key.Enter && it.type == KeyEventType.KeyUp) -> {
-                            history.add(textValue)
-                            if (tts.value?.isSpeaking == true) {
-                                tts.value?.stop()
-                                isSpeaking = false
-                            } else {
-                                tts.value?.speak(
-                                    textValue, TextToSpeech.QUEUE_FLUSH, emptyMap(), ""
-                                )
-                                isSpeaking = true
-                            }
-                            prevTextValue = textValue
-                            textValue = ""
-                            true
-                        }
-                        (it.key == Key.Enter && it.type == KeyEventType.KeyUp) -> {
-                            true
-                        }
-                        else -> false
-                    }
+            CompositionLocalProvider(
+                // You can also provides null to completely disable the default input service.
+                LocalTextInputService provides null
+            ) {
+                val focusRequester = remember { FocusRequester() }
+                LaunchedEffect(Unit) {
+                    focusRequester.requestFocus()
                 }
-            )
+                OutlinedTextField(
+                    value = textValue,
+                    onValueChange = { newText: String ->
+                        textValue = newText
+                    },
+                    textStyle = textStyle,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color.Transparent,
+                        disabledBorderColor = Color.Transparent,
+                        errorBorderColor = Color.Transparent,
+                        unfocusedBorderColor = Color.Transparent,
+                        cursorColor = Color.Transparent,
+                    ),
+                    placeholder = {
+                        Text(
+                            text = prevTextValue,
+                            style = textStyle,
+                            textAlign = TextAlign.Center,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            modifier = Modifier.fillMaxWidth(),
+                        )
+                    },
+
+                    modifier = Modifier.onPreviewKeyEvent {
+                        when {
+                            (it.key == Key.Enter && it.type == KeyEventType.KeyUp) -> {
+                                history.add(textValue)
+                                if (tts.value?.isSpeaking == true) {
+                                    tts.value?.stop()
+                                    isSpeaking = false
+                                } else {
+                                    tts.value?.speak(
+                                        textValue, TextToSpeech.QUEUE_FLUSH, emptyMap(), ""
+                                    )
+                                    isSpeaking = true
+                                }
+                                prevTextValue = textValue
+                                textValue = ""
+                                true
+                            }
+                            (it.key == Key.Enter && it.type == KeyEventType.KeyUp) -> {
+                                true
+                            }
+                            else -> false
+                        }
+                    }.focusRequester(focusRequester)
+                )
+            }
             Spacer(Modifier.weight(1f))
         }
     }
